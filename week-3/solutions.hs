434f92a065111d8847ddebed05169320d0bc7ef2
Brad Moore <bradleyjm2001@gmail.com>
2024-02-11 18:58:25 +0000

    more week 3 solutions

--- week-3/solutions.hs
+++ week-3/solutions.hs
@@ -1,5 +1,7 @@
 import Data.Time.Calendar.MonthDay (monthAndDayToDayOfYear)
-import Prelude hiding ((&&))
+import GHC.Float (sqrtFloat)
+import Text.XHtml (above, base)
+import Prelude hiding (gcd, (&&))
 
 -- (&&) :: Bool -> Bool -> Bool
 -- True && True = True
@@ -34,3 +36,28 @@ daysInMonth month
   | month == 2 = 28
   | month == 4 || month == 6 || month == 9 || month == 11 = 30
   | month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 = 31
+
+-- Don't need to check for negative input as question specifices input
+-- is non-negative
+sumNumbers :: Int -> Int
+sumNumbers n | n == 0 = 0 | otherwise = n + sumNumbers (n - 1)
+
+sumSquares :: Int -> Int
+sumSquares n | n == 0 = n | otherwise = n ^ 2 + sumSquares (n - 1)
+
+power :: Int -> Int -> Int
+power n exponent | exponent == 0 = 1 | otherwise = n * power n (exponent - 1)
+
+sumFromTo :: Int -> Int -> Int
+sumFromTo a b
+  | a > b = 0
+  | a == b = a
+  | otherwise = a + sumFromTo (a + 1) b
+
+-- Don't have to account for non-negative
+-- Would be edge cases if we did
+gcd :: Int -> Int -> Int
+gcd a b
+  | a == b = a
+  | a > b = gcd (a - b) b
+  | otherwise = gcd (b - a) a
