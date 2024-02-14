f2bbc44cbdcf9cb37147dcbc1ced30701fef805b
Brad Moore <bradleyjm2001@gmail.com>
2024-02-14 18:45:28 +0000

    start week 4

---
+++ week-4/solutions.hs
@@ -0,0 +1,58 @@
+import Data.Char
+
+type StudentMark = (String, Int)
+
+sumDifference :: Int -> Int -> (Int, Int)
+sumDifference a b = (a + b, a - b)
+
+-- Fix
+grade :: StudentMark -> Char
+grade (name, mark)
+  | mark >= 70 = 'A'
+  | mark >= 60 = 'B'
+  | mark >= 50 = 'C'
+  | mark >= 40 = 'D'
+  | mark < 40 = 'F'
+  | mark < 0 || mark > 100 = error "Invalid mark"
+
+-- Fix
+capMark :: StudentMark -> StudentMark
+capMark (name, mark)
+  | mark >= 40 = (name, 40)
+  | mark < 40 && mark >= 0 = (name, mark)
+  | otherwise = error "Invalid mark"
+
+firstNumbers :: Int -> [Int]
+firstNumbers n = [1 .. n]
+
+firstSquares :: Int -> [Int]
+firstSquares n = [i ^ 2 | i <- firstNumbers n]
+
+capitalise :: String -> String
+capitalise str = [toUpper i | i <- str]
+
+onlyDigits :: String -> String
+onlyDigits str = [i | i <- str, isDigit i]
+
+testData :: [StudentMark]
+testData =
+  [ ("John", 53),
+    ("Sam", 16),
+    ("Kate", 85),
+    ("Jill", 65),
+    ("Bill", 37),
+    ("Amy", 22),
+    ("Jack", 41),
+    ("Sue", 71)
+  ]
+
+capMarks :: [StudentMark] -> [StudentMark]
+capMarks studentList = [capMark i | i <- studentList]
+
+gradeStudents :: [StudentMark] -> [(String, Char)]
+gradeStudents studentList = [(i, grade (i, j)) | (i, j) <- studentList]
+
+duplicate :: String -> Int -> String
+duplicate str count
+  | count == 1 = str
+  | otherwise = str ++ duplicate str (count - 1)
